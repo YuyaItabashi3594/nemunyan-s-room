87391f56ed1b99c824f4af0aee8e42df414848f2
YuyaItabashi3594 <asuholover@gmail.com>
2023-07-12 14:55:17 +0900

    Updates

--- _nuxt/DocumentDrivenNotFound.1bf1c050.js
+++
@@ -1,1 +0,0 @@
-import{q as n,x as e}from"./entry.e930e743.js";const t=n({name:"DocumentDrivenNotFound",render(){return e("div","Document not found")}});export{t as default};
---
+++ aboutmd/iidx.md
@@ -0,0 +1,8 @@
+## beatmania IIDXとは？
+
+KONAMiが開発・販売している音楽ゲームのシリーズ。
+![beatmania IIDX](/pictures/iidxlm.jpg)
+
+## Double Playとは？
+
+14個の鍵盤と2つのスクラッチを使ってプレイするモード。
